3a893ec5ccc3098c4ab7950ea92a9d0de8efd174
Hayet Kumasseh <naaakleh@gmail.com>
2022-05-22 05:08:44 -0400

    commit tree 1cce5830ea6bc8ad220a5030859b05dd0bb3ef05

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Write your solution in this file!
+const employee = {
+    name: 'Tee',
+    streetAddress : '3 Bergen Street '
+}
+
+beforeEach(function () {
+    for (const key in employee) {
+      delete employee[key];
+    }
+
+    employee.name = 'Sam';
+  });
+
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newEmployee = {...employee};
+    newEmployee[key] = value;
+    return newEmployee;
+}
+// updateEmployeeWithKeyAndValue()
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    let newE = employee;
+    newE[key] = value;
+    return newE;
+}
+
+// destructivelyUpdateEmployeeWithKeyAndValue()
+
+function deleteFromEmployeeByKey(employee, key) {
+    let newObject = {...employee}
+    delete newObject[key];
+    return newObject
+}
+
+
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    let newObject = employee
+    delete newObject[key];
+    return newObject
+}
+
